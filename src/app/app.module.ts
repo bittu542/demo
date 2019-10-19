e0c607025086e45bff61ca8616b2cd97e20b458a
= <bittubittu037@gmail.com>
2019-10-19 22:20:03 +0530

    updated to upload data to heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { FormsModule } from '@angular/forms';
     AppComponent,
     NewCompComponent,
     ChangeTextDirective,
-    SqrtPipe 
+    SqrtPipe
   ],
   imports: [
     BrowserModule,
